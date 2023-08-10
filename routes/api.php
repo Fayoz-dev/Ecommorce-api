7f7faae2ac0394f966335428d84e52b26b727cb4
Fayoz-dev <frasulov988@gmail.com>
2023-08-10 18:43:41 +0500

    favorites

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CategoryProductController;
+use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,8 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login',[AuthController::class,'login']);
+Route::post('register',[AuthController::class]);
+
 Route::apiResources([
    'categories' => CategoryController::class,
    'products' => ProductController::class,
-   'categories.products' => CategoryProductController::class
+   'categories.products' => CategoryProductController::class,
+   'favorites' => FavoriteController::class
 ]);
